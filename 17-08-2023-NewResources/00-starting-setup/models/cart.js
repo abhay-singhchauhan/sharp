ff94c21034328185b46e7a7effbe269c30d97d0e
abhay singh chauhan <itsyourabhay@gmail.com>
2023-08-18 15:48:01 +0530

    added edit functionality

--- 17-08-2023-NewResources/00-starting-setup/models/cart.js
+++ 17-08-2023-NewResources/00-starting-setup/models/cart.js
@@ -1,33 +1,38 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  "data",
-  "cart.json"
+  'data',
+  'cart.json'
 );
 
 module.exports = class Cart {
-  static addProduct(id, price) {
+  static addProduct(id, productPrice) {
+    // Fetch the previous cart
     fs.readFile(p, (err, fileContent) => {
       let cart = { products: [], totalPrice: 0 };
-
       if (!err) {
-        JSON.parse(fileContent);
+        cart = JSON.parse(fileContent);
       }
-      const productExistsInd = cart.products.find((prod) => prod.id === id);
-      const productExists = cart.products[productExistsInd];
+      // Analyze the cart => Find existing product
+      const existingProductIndex = cart.products.findIndex(
+        prod => prod.id === id
+      );
+      const existingProduct = cart.products[existingProductIndex];
       let updatedProduct;
-      if (productExists) {
-        updatedProduct = { ...productExists };
-        updatedProduct.qty++;
+      // Add new product/ increase quantity
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
         cart.products = [...cart.products];
-        cart.products[productExistsInd] = updatedProduct;
+        cart.products[existingProductIndex] = updatedProduct;
       } else {
         updatedProduct = { id: id, qty: 1 };
         cart.products = [...cart.products, updatedProduct];
       }
-      cart.totalPrice = cart.totalPrice + price;
-      fs.writeFile(p, JSON.stringify(cart), (err) => {
+      cart.totalPrice = cart.totalPrice + +productPrice;
+      fs.writeFile(p, JSON.stringify(cart), err => {
         console.log(err);
       });
     });
